b2925ad9ee2c5723333b13935645a76197adc31f
Kiara Manacasa <kmanacasa@gmail.com>
2025-11-07 10:37:35 -0300

    arreglo de notas de variables globales

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c(
+  ".data",
+  "fecha", "id", "mes", "temperatura_abrigo_150cm", "promedio",
+  "temperatura_abrigo_150cm_maxima", "temperatura_abrigo_150cm_minima",
+  "valor", "media", "desvio", "maximo", "minimo", "estadistica"
+))
